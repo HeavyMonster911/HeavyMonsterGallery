b4dc403264efa67c5216063340b387c6335d09d3
HeavyMonster911 <72610595+HeavyMonster911@users.noreply.github.com>
2021-05-06 22:44:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ arrowTop.onclick = function() {
 window.addEventListener('scroll', function() {
   arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
 });
+
+let menuBar = document.querySelector('.menuBar');
+let navigation = document.querySelector('.navigation');
+let closeBar = document.querySelector('.closeBar');
+menuBar.onclick = function() {
+  this.classList.add('activeBtn');
+  closeBar.classList.add('disableBtn')
+  navigation.classList.add('active');
+}
+closeBar.onclick = function() {
+  this.classList.remove('disableBtn');
+  menuBar.classList.remove('activeBtn')
+  navigation.classList.remove('active');
+}
